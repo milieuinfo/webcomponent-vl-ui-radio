67dab5be2c39c073236187dd0426b02dd2beec1a
coemans <tom.coemans@gmail.com>
2020-03-30 11:18:54 +0200

    Introductie radio-group element (#49)

---
+++ test/e2e/pages/vl-radio-group.page.js
@@ -0,0 +1,18 @@
+const VlRadio = require('../components/vl-radio');
+const { Page, Config } = require('vl-ui-core').Test;
+
+class VlRadioPage extends Page {
+  async _getRadio(selector) {
+    return new VlRadio(this.driver, selector);
+  }
+
+  async getRadio(number) {
+    return this._getRadio(`#radio-${number}`);
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-radio-group.html');
+  }
+}
+
+module.exports = VlRadioPage;
