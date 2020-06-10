a39624e72cb83a7cb2c725ef3dccbfaa4b45076c
Tom Coemans <tom.coemans@gmail.com>
2020-06-10 20:38:23 +0200

    no JSDoc support for mixin

--- src/vl-radio.js
+++ src/vl-radio.js
@@ -6,7 +6,6 @@ import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
  * @classdesc De radio laat de gebruiker toe om een enkele optie te selecteren uit een lijst. Gebruik de radio in formulieren. Vermijd een voorgedefinieerde keuze vast te leggen om de gebruiker een bewuste keuze te laten maken.
  *
  * @extends HTMLElement
- * @mixin vlElement
  *
  * @property {boolean} data-vl-block - Attribuut wordt gebruikt om ervoor te zorgen dat de radio getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen.
  * @property {boolean} data-vl-checked - Attribuut wordt gebruikt om de radio standaard te selecteren.
