d24701011faf9c4b096742855eb3e33ceec30526
coemans <tom.coemans@gmail.com>
2020-08-14 15:06:04 +0200

    Upgrade huisstijl versie 3.11.5 (#80)

--- src/vl-radio-group.js
+++ src/vl-radio-group.js
@@ -6,6 +6,7 @@ import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
  * @classdesc De radio group bundelt verschillende radio elementen die samenhoren.
  *
  * @extends HTMLElement
+ * @mixes vlElement
  *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-radio/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-radio/issues|Issues}
