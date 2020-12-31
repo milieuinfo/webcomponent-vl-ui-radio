0bd581c9355ad475f6a878db2c1696b82002b85f
Tom Coemans <tom.coemans@gmail.com>
2020-12-31 11:01:28 +0100

    only attach internals when it is supported

--- src/vl-radio.js
+++ src/vl-radio.js
@@ -51,7 +51,9 @@ export class VlRadio extends vlElement(HTMLElement) {
       </label>
     `);
     Object.assign(this, vlRadioGroup);
-    this._internals = this.attachInternals();
+    if (this.attachInternals) {
+      this._internals = this.attachInternals();
+    }
   }
 
   connectedCallback() {
@@ -76,7 +78,7 @@ export class VlRadio extends vlElement(HTMLElement) {
    * @return {HTMLFormElement}
    */
   get form() {
-    return this._internals.form;
+    return this._internals?.form;
   }
 
   /**
@@ -85,7 +87,7 @@ export class VlRadio extends vlElement(HTMLElement) {
    * @return {ValidityState}
    */
   get validity() {
-    return this._internals.validity;
+    return this._internals?.validity;
   }
 
   /**
@@ -94,7 +96,7 @@ export class VlRadio extends vlElement(HTMLElement) {
    * @return {string}
    */
   get validationMessage() {
-    return this._internals.validationMessage;
+    return this._internals?.validationMessage;
   }
 
   /**
@@ -103,7 +105,7 @@ export class VlRadio extends vlElement(HTMLElement) {
    * @return {boolean}
    */
   get willValidate() {
-    return this._internals.willValidate;
+    return this._internals?.willValidate;
   }
 
   /**
